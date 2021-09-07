407db614281b2fc28e347ea2ac0c79369bd81400
the-torn <the-torn@users.noreply.github.com>
2021-09-06 23:22:28 -0700

    Add Floot home page

--- src/index.html.jsx
+++ src/index.html.jsx
@@ -3,9 +3,15 @@ import About from './pages/about.html.mdx';
 export default () => {
   return (
     <>
-      <h1>Welcome to my new Charge site!</h1>
-      <About />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link href="https://fonts.googleapis.com/css2?family=EB+Garamond&display=swap" rel="stylesheet" />
+
+      <div className="header"></div>
+      <main>
+        <About />
+      </main>
+      <link href="/stylesheets/index.css" rel="stylesheet" />
     </>
   );
 };
-
